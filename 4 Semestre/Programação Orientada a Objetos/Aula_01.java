1d698f3f640c953a6b6b5793b700f5a7c1453e1b
Lord Tsuki <xyzgames@outlook.com.br>
2022-08-21 00:50:05 -0300

    Create Aula_01.java
    Aula 1 Java

---
+++ 4 Semestre/Programação Orientada a Objetos/Aula_01.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Aula_01
+{
+    public static void main (String[] args)
+    {
+        System.out.println("<Batata frita>");
+        System.out.println(1);
+        System.out.println(7.4);
+        System.out.println(2 + 2);
+        System.out.println("Soma de 2 + 2 " + (2 + 2));
+        System.out.println("Pedro nasceu em " + 1980 + " e tem " + (2010 - 1980) + "anos");
+    }
+}
