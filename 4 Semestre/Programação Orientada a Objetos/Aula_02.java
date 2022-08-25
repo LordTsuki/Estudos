bf15d1a0e448778924eef2ffc10ba5bb68fbd687
Lord Tsuki <xyzgames@outlook.com.br>
2022-08-25 20:30:42 -0300

    Aula 2

---
+++ 4 Semestre/Programação Orientada a Objetos/Aula_02.java
@@ -0,0 +1,15 @@
+public class Aula_02 
+{
+    public static void main(String[] args) throws Exception
+    {
+        boolean resultado;
+        resultado = true;
+        resultado = 10 > 90;
+        int n = 10;
+        int m = 22;
+
+        resultado = n <= m;
+
+        System.out.println("Valor lógico: " + resultado);
+    }    
+}
